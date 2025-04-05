9dc17b7dd6d8b4b5a3551be881c15410118b4a47
unknown <sabin@epraivid.com>
2025-04-05 12:00:57 +0545

    change projects section

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Projects from "./components/Projects"
 const App = () => {
   return (
     <div className="dark:bg-zinc-900 dark:text-zinc-50 w-full min-h-screen md:py-[3vh] py-10 flex items-start justify-center">
-      <div className="lg:w-[50rem] w-full lg:px-0 px-6 h-full flex flex-col gap-4">
+      <div className="lg:w-[55rem] w-full lg:px-0 px-6 h-full flex flex-col gap-4">
         <Header />
         <About />
         <Skills />
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,12 @@
 import { motion } from "framer-motion"
 import { fadeIn } from "../utils/animation.ts"
-import { IconGitMerge } from "@tabler/icons-react"
+import { Github, PencilLine } from "lucide-react"
+import Sabinui from "../assets/sabinui.png"
+import OrganicKarnali from "../assets/organickarnali.png"
+import SwiftOps from "../assets/swiftops.png"
+import Crypto from "../assets/crypto.png"
+import Trendora from "../assets/trendora.png"
+import Portfolio from "../assets/portfolio.png"
 
 const Projects = () => {
 
@@ -8,47 +14,52 @@ const Projects = () => {
         {
             title: "Sabin UI",
             description: "A UI library for React & vanilla JavaScript",
+            techstack: "React / TailwindCSS / Framer",
+            image: Sabinui,
             sourceCode: "https://github.com/maybesabin/SabinUI",
             livePreview: "https://sabinui.vercel.app/"
         },
         {
             title: "Organic Karnali",
-            description: "e-commerce site with cart & checkout functionality",
+            description: "An e-commerce site with cart & checkout functionality",
+            techstack: "Next js / TailwindCSS / Framer / Shadcn UI",
+            image: OrganicKarnali,
             sourceCode: "https://github.com/E-pravidi-Web-Dev/organickarnali_frontend",
             livePreview: "https://organickarnalifrontend.web.app/"
         },
         {
             title: "SwiftOps",
             description: "An AI SaaS-themed landing page",
+            techstack: "React / TailwindCSS / Shadcn UI",
+            image: SwiftOps,
             sourceCode: "https://github.com/maybesabin/SwiftOps",
             livePreview: "https://swiftops.vercel.app/"
         },
         {
             title: "Crypto Landing Page",
             description: "A crypto app landing page",
+            techstack: "React / TailwindCSS / GSAP",
+            image: Crypto,
             sourceCode: "https://github.com/maybesabin/Crypto-Website",
             livePreview: "https://crypto-website-sabin.vercel.app/"
         },
         {
             title: "Trendora",
             description: "An e-commerce website",
+            techstack: "React / TailwindCSS / Context API",
+            image: Trendora,
             sourceCode: "https://github.com/maybesabin/Trendora-Ecommerce",
             livePreview: "https://trendora-shop.vercel.app/"
         },
         {
-            title: "Apple Academy",
-            description: "A website for a local Montessori school",
-            sourceCode: "https://github.com/maybesabin/Apple-Academy",
-            livePreview: "https://apple-academy-iota.vercel.app/"
-        },
-        {
             title: "Old Portfolio",
             description: "my old portfolio website",
+            techstack: "React / TailwindCSS / GSAP",
+            image: Portfolio,
             sourceCode: "https://github.com/maybesabin/Portfolio-Latest",
             livePreview: "https://sxbin.netlify.app/"
         }
     ];
-
 
     return (
         <div className="w-full flex flex-col items-start gap-3 mt-4">
@@ -59,25 +70,33 @@ const Projects = () => {
                 className="md:text-xl text-base font-semibold">Projects
             </motion.h2>
 
-            <div className="flex flex-col gap-3 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
                 {projects.map((project, index) => (
                     <motion.div
                         initial={fadeIn.initial}
                         animate={fadeIn.animate}
                         transition={{ ...fadeIn.transition, delay: 0.17 * (index + 1) }}
                         key={index}
-                        className="w-full flex justify-between md:items-start items-center gap-2"
+                        className="flex flex-col items-start gap-2 w-full"
                     >
-                        <div className="flex md:flex-row flex-col gap-1">
-                            <a href={project.livePreview} target="_blank" className="md:text-sm text-xs transition-all">
-                                {index + 1}.&nbsp; <span className="hover:text-yellow-400 transition-all underline cursor-pointer">{project.title}&nbsp;</span>
+                        <img src={project.image} className="brightness-75 hover:brightness-100 transition-all object-cover w-full md:h-72 h-56 rounded-lg" alt="" />
+                        <h3 className="md:text-base text-sm font-medium mt-2">{project.title}</h3>
+                        <h4 className="text-[0.7rem] text-[#25dde5] -mt-2">
+                            {project.techstack}
+                        </h4>
+                        <p className="my-1.5 text-xs text-neutral-300 font-light">
+                            {project.description}
+                        </p>
+                        <div className="flex items-center gap-5">
+                            <a target="_blank" href={project.livePreview} className="cursor-pointer flex items-center gap-2 text-[#25dde5] hover:text-[#58ecf4]">
+                                <PencilLine className="md:size-[15px] size-[12px]" />
+                                <h6 className="text-xs">Live Preview</h6>
                             </a>
-                            <p className="md:text-sm text-xs text-neutral-400 lowercase">{project.description}</p>
+                            <a target="_blank" href={project.sourceCode} className="cursor-pointer flex items-center gap-2 text-[#25dde5] hover:text-[#58ecf4]">
+                                <Github className="md:size-[15px] size-[12px]" />
+                                <h6 className="text-xs">Repo Url</h6>
+                            </a>
                         </div>
-                        <a href={project.sourceCode} target="_blank">
-                            <IconGitMerge className="md:size-4 size-3 cursor-pointer hover:text-yellow-400 transition-all" title="Source Code" />
-                        </a>
-
                     </motion.div>
                 ))}
             </div>

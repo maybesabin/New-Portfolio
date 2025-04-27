d74e0c8328c44823866c0adc0ba3338e85ce3242
unknown <highsabin987@gmail.com>
2025-04-27 20:58:44 +0545

    chore: fix null case in fetching data

--- frontend/src/components/Github.tsx
+++ frontend/src/components/Github.tsx
@@ -2,22 +2,29 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion"
 import { fadeIn } from "@/utils/animation";
+import { infinity } from 'ldrs'
 
 const Github = () => {
 
+    infinity.register();
     const username = "maybesabin";
     const url = `https://github.com/${username}`
     const [contributions, setContributions] = useState<null | number>(null);
     const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false)
 
     const fetchContributions = async () => {
+        setLoading(true)
         try {
             const response = await axios.get(`https://github-contributions-api.jogruber.de/v4/${username}`);
             setContributions(response.data.total["2025"])
             setError(null);
+            setLoading(false)
         } catch (error: any) {
             setError("Failed to fetch contributions.");
             console.log(error.message)
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -32,7 +39,20 @@ const Github = () => {
             target="_blank"
             href={url}
             className="text-xs text-neutral-500 hover:text-neutral-400 cursor-pointer mt-2 transition-all">
-            {error ? error : `${contributions} contributions this year`}
+            {
+                (loading || contributions == null) ?
+                    <l-infinity
+                        size="25"
+                        stroke="2"
+                        stroke-length="0.15"
+                        bg-opacity="0.1"
+                        speed="1.3"
+                        color="white"
+                    ></l-infinity>
+                    :
+                    `${contributions} contributions this year`
+            }
+            {(!loading && error) && 'failed to fetch data'}
         </motion.a>
     )
 }
--- frontend/src/components/Wakatime.tsx
+++ frontend/src/components/Wakatime.tsx
@@ -2,21 +2,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion"
 import { fadeIn } from "@/utils/animation";
+import { infinity } from 'ldrs'
 
 const Wakatime = () => {
-
+    infinity.register()
     const wakatimeUrl = 'https://wakatime.com/@sabinhamal_'
     const [totalTime, setTotalTime] = useState<null | any>(null);
     const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
 
     const fetchContributions = async () => {
+        setLoading(true)
         try {
             const response = await axios.get(`${import.meta.env.VITE_BACKEND_URI}/api/wakatime`);
             setTotalTime(response.data?.data[0]?.grand_total?.text)
             setError(null);
+            setLoading(false);
         } catch (error: any) {
             setError("Failed to fetch contributions.");
             console.log(error.message)
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -31,7 +37,23 @@ const Wakatime = () => {
             target="_blank"
             href={wakatimeUrl}
             className="text-xs text-neutral-500 hover:text-neutral-400 cursor-pointer mt-2 transition-all">
-            {error ? 'failed to fetch data' : (totalTime == null || totalTime == '0 secs') ? 'no coding today :(' : `i coded ${totalTime} today`}
+
+            {
+                loading ?
+                    <l-infinity
+                        size="25"
+                        stroke="2"
+                        stroke-length="0.15"
+                        bg-opacity="0.1"
+                        speed="1.3"
+                        color="white"
+                    ></l-infinity>
+                    :
+                    (totalTime == null || totalTime == `0 secs`) ?
+                        'no coding today :(' :
+                        `i coded ${totalTime} today`
+            }
+            {(!loading && error) && 'failed to fetch data'}
         </motion.a>
     )
 }
